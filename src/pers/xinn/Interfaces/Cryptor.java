af79d75ddbc23fdb40ab706fd45e39915a5e9295
ZhaoXinn <410580169@gapp.fju.edu.tw>
2022-03-16 03:53:37 +0800

    Initial commit Mostly underconstruction,unpublished

---
+++ src/pers/xinn/Interfaces/Cryptor.java
@@ -0,0 +1,4 @@
+package pers.xinn.Interfaces;
+
+public class Cryptor {
+}
---
+++ src/pers/xinn/Test.java
@@ -0,0 +1,19 @@
+package pers.xinn;
+
+import pers.xinn.util.StringToList;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+        StringToList s = new StringToList("ABC");
+
+        s.println();
+        s.printspaced();
+
+
+
+    }
+
+
+}
